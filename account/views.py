890a1d31a786ddb928925e741d28cd1defa82791
Samin Rahman <55238473+hornet67@users.noreply.github.com>
2026-05-09 19:08:52 +0600

    working

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 from .models import Useraccount
 
 
@@ -11,8 +11,6 @@ from .models import Useraccount
 class SignupView(TemplateView):
     template_name = "signup.html"
 
-def Login(request):
-    return render(request, 'login.html')
 
 class LoginView(TemplateView):
     template_name = "login.html"
@@ -23,7 +21,7 @@ class ForgetPasswordView(TemplateView):
 class ResetPasswordView(TemplateView):
     template_name = "reset_password.html"
     
-class PracticeView(TemplateView):
-    model = "Useraccount"
+class PracticeView(ListView):
+    model = Useraccount
     template_name = "practice.html"
     context_object_name = "users"
